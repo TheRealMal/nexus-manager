ee8a0f5b5322b71b7b8b24e7805d0fbfc16ca9ef
therealmal <therealmal23@gmail.com>
2023-08-04 00:28:01 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "Nexus Manager",
-    version = "0.0.2",
+    version = "0.0.3",
     description = "Sonatype Nexus Raw repositories manager",
     author = "therealmal",
     author_email = "therealmal23@gmail.com",
